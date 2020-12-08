7ccbb37251f65788f4875d9f2217efaeb311bb20
Matheus Bastos <matheusbastos@outlook.com>
2020-12-08 01:33:07 -0300

    add features

--- libraries/System.php
+++ libraries/System.php
@@ -4,5 +4,41 @@ namespace libraries;
 
 class System extends Router {
 
+  private $url;
+  private $exploder;
+  private $area;
+  private $controller;
+  private $action;
+  private $params;
+
+  public function setUrl() {
+    $this->url = isset($_GET['url']) ? $_GET['url'] : 'home/index';
+  }
+
+  private function setExploder() {
+    $this->exploder = explode('/', $this->url);
+  }
+
+  /**
+   *
+   */
+  private function setArea() {
+
+    foreach($this->routers as $index => $value) {
+
+      if ($this->onDefault && $this->exploder[0] === $index) {
+        $this->area = $value;
+        $this->onDefault = false;
+      }
+
+    }
+
+    $this->area = empty($this->area) ? $this->defaultRouter : $this->area;
+
+    if (!defined('APP_AREA')) {
+      define("APP_AREA", $this->area);
+    }
+
+  }
 
 }
